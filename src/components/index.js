30803f76063f6dc53c949a5a4a0eaebabaedfd0d
Seth Davis <sethdavis512@gmail.com>
2019-04-08 23:05:41 -0500

    Reorganize package, add scripts.

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Button } from './Button';
+export { default as InputText } from './InputText';
+export { default as ProgressBar } from './ProgressBar';
+export { default as Select } from './Select';
